1182c8e5721ce2893cc18f995b5014369fff4b12
Diego Prada-Gracia <prada.gracia@gmail.com>
2025-03-05 00:26:50 -0600

    Removing minimizing reporter for OpenMM

--- molsysmt/thirds/openmm/reporters/__init__.py
+++ molsysmt/thirds/openmm/reporters/__init__.py
@@ -1,4 +1,3 @@
 from .StructuresDict import StructuresDictReporter
-from .StructuresDictMinimizer import StructuresDictMinimizerReporter
 from .tqdm import TQDMReporter
 from .h5msm import H5MSMReporter
