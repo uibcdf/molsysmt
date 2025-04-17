5bdef375dcc0a194693b03ef53b5f315d1e6410e
Diego Prada-Gracia <prada.gracia@gmail.com>
2025-04-16 23:10:05 -0600

    In process to redefine the native topology

--- molsysmt/element/component/get_component_name.py
+++ molsysmt/element/component/get_component_name.py
@@ -120,3 +120,87 @@ def get_component_name(molecular_system, element='atom', selection='all', redefi
                      component_name=True)
 
     return output
+
+
+### from native topology
+
+#from molsysmt.element.group.small_molecule import group_names as small_molecule_names
+#from molsysmt.element.group.saccharide import group_names as saccharide_names
+
+#aux_df = self.groups.groupby('component_index').agg(group_name=('group_name', list),
+#                                                    group_type=('group_type', list))
+
+#component_types = self.components['component_type'].to_numpy()
+
+#counter = {'peptide':0, 'protein':0, 'small molecule':0, 'saccharide':0, 'unknown':0}
+
+#peptides = {}
+#proteins = {}
+#small_molecules = {}
+#saccharides = {}
+
+#for component_type, row in zip(component_types, aux_df.itertuples(index=True)):
+#
+#    if component_type == 'peptide':
+
+#        string_peptide = ','.join(row.group_name)
+
+#        if string_peptide in peptides:
+#            component_name = peptides[string_peptide]
+#        else:
+#            component_name = component_type+' '+str(counter[component_type])
+#            peptides[string_peptide] = component_name
+#            counter[component_type] += 1
+
+#    elif component_type == 'protein':
+
+#        string_protein = ','.join(row.group_name)
+
+#        if string_protein in proteins:
+#            component_name = proteins[string_protein]
+#        else:
+#            component_name = component_type+' '+str(counter[component_type])
+#            proteins[string_protein] = component_name
+#            counter[component_type] += 1
+
+#    elif component_type == 'small molecule':
+
+#        group_name = row.group_name[0]
+
+#        if group_name in small_molecules:
+#            component_name = small_molecules[group_name]
+#        else:
+#            if group_name in small_molecule_names:
+#                component_name = group_name
+#            else:
+#                component_name = group_name
+#            small_molecules[component_name] = component_name
+
+#    elif component_type == 'saccharide':
+
+#        group_name = row.group_name[0]
+
+#        if group_name in saccharides:
+#            component_name = saccharides[group_name]
+#        else:
+#            if group_name in saccharide_names:
+#                component_name = group_name
+#            else:
+#                component_name = group_name
+#            saccharides[component_name] = component_name
+
+#    elif component_type in ['ion', 'lipid']:
+
+#        component_name = row.group_name[0]
+
+#    elif component_type in ['water']:
+
+#        component_name = 'water'
+
+#    else:
+
+#        component_name = 'unknown '+str(counter['unknown'])
+#        counter['unknown']+=1
+
+#    self.components.iloc[row.Index,1] = component_name
+
