f54d2c9451a2af1c001751b6cc4d989508f86c15
Diego Prada <prada.gracia@gmail.com>
2025-09-17 15:00:54 -0600

    Working in the documentation

---
+++ molsysmt/form/file_bcif/to_bcifreader_PdbxContainers_DataContainer.py
@@ -0,0 +1,16 @@
+from molsysmt._private.digestion import digest
+
+@digest(form='file:cif')
+def to_bcifreader_PdbxContainers_DataContainer(item, atom_indices='all', skip_digestion=False):
+
+    from bcifreader import BinaryCifReader
+
+    binary_cif_reader = BinaryCifReader()
+    containers = binary_cif_reader.deserialize(item)
+
+    if len(containers)>1:
+        print('Warning! The PDB ID has more than a DataContainer')
+
+    tmp_item = containers[0]
+
+    return tmp_item
