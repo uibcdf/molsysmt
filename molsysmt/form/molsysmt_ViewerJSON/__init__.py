c545b788ec911dedc4eca7f6d94d60f8d53da3dd
Diego Prada <prada.gracia@gmail.com>
2025-11-21 17:04:19 -0600

    Ensure JSON form modules export form metadata

--- molsysmt/form/molsysmt_ViewerJSON/__init__.py
+++ molsysmt/form/molsysmt_ViewerJSON/__init__.py
@@ -8,6 +8,27 @@ piped_any_attribute = None
 bonds_are_explicit = True
 bonds_can_be_computed = False
 
+__all__ = [
+    'form_name',
+    'form_type',
+    'form_info',
+    'piped_topological_attribute',
+    'piped_structural_attribute',
+    'piped_any_attribute',
+    'bonds_are_explicit',
+    'bonds_can_be_computed',
+    'is_form',
+    'attributes',
+    'has_attribute',
+    'get',
+    'copy',
+    'extract',
+    'append_structures',
+    'to_molsysmt_ViewerJSON',
+    'to_molsysmt_MolSys',
+    '_convert_to',
+]
+
 from .is_form import is_form
 from .attributes import attributes
 from .has_attribute import has_attribute
