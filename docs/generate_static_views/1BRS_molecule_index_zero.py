701e4cd718c07a748a3592ed434b66a4ec53cbbb
Diego Prada <prada.gracia@gmail.com>
2026-01-31 15:18:20 -0600

    In process

--- docs/generate_static_views/1BRS_molecule_index_zero.py
+++ docs/generate_static_views/1BRS_molecule_index_zero.py
@@ -1,12 +1,7 @@
-from pathlib import Path
-
 import molsysmt as msm
-import molsysviewer as viewer
 
 molsys = msm.convert(msm.systems['Barnase-Barstar']['1brs.bcif.gz'],
                      selection='molecule_type=="protein"',
                      to_form='molsysmt.MolSys')
-
-view = viewer.make_viewer(molsys, selection='molecule_index==0')
-view.show()
+view = msm.view(molsys, selection='molecule_index==0')
 view.write_html("../_static/views/1BRS_molecule_index_zero.html", title="1BRS Molecule Index 0", mode="lite")
