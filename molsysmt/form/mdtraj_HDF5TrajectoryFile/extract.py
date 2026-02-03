46f3cf6f7877afee8d568ea93ac6a64da862661b
Diego Prada-Gracia <prada.gracia@gmail.com>
2026-02-02 22:55:53 -0600

    Apply @requires decorator to extract.py in all soft-dependency forms (MDTraj, Parmed, Pytraj, MDAnalysis, NGLView, Pdbfixer, Biopython, MMTF)

--- molsysmt/form/mdtraj_HDF5TrajectoryFile/extract.py
+++ molsysmt/form/mdtraj_HDF5TrajectoryFile/extract.py
@@ -1,8 +1,10 @@
 from molsysmt._private.exceptions import NotImplementedMethodError
 from molsysmt._private.digestion import digest
 from molsysmt._private.variables import is_all
+from molsysmt.dependencies import requires
 
 @digest(form='mdtraj.HDF5TrajectoryFile')
+@requires('mdtraj')
 def extract(item, atom_indices='all', structure_indices='all', output_filename=None, copy_if_all=True,
         progress_bar=False, skip_digestion=False):
 
