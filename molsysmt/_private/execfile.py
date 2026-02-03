2a68641340ac775b73eb77078847aa0ec10d7790
Diego Prada-Gracia <prada.gracia@gmail.com>
2026-02-02 23:38:30 -0600

    Perf: cache compiled code in execfile to speed up repeated common code loading

--- molsysmt/_private/execfile.py
+++ molsysmt/_private/execfile.py
@@ -1,6 +1,8 @@
 ### From debugpy/_vendored/pydevd/_pydev_bundle/_pydev_execfile.py
 ### I don't know why, but jupyter loads it by default. Python and IPython does not load it.
 
+_CACHE = {}
+
 def execfile(file, glob=None, loc=None):
     if glob is None:
         import sys
@@ -8,21 +10,27 @@ def execfile(file, glob=None, loc=None):
     if loc is None:
         loc = glob
 
-    # It seems that the best way is using tokenize.open(): http://code.activestate.com/lists/python-dev/131251/
-    # (but tokenize.open() is only available for python 3.2)
-    import tokenize
-    if hasattr(tokenize, 'open'):
-        # version 3.2
-        stream = tokenize.open(file)  # @UndefinedVariable
-    else:
-        # version 3.0 or 3.1
-        detect_encoding = tokenize.detect_encoding(open(file, mode="rb").readline)
-        stream = open(file, encoding=detect_encoding[0])
-    try:
-        contents = stream.read()
-    finally:
-        stream.close()
+    import os
+    abs_path = os.path.abspath(file)
 
-    # execute the script (note: it's important to compile first to have the filename set in debug mode)
-    exec(compile(contents + "\n", file, 'exec'), glob, loc)
+    if abs_path not in _CACHE:
+        # It seems that the best way is using tokenize.open(): http://code.activestate.com/lists/python-dev/131251/
+        # (but tokenize.open() is only available for python 3.2)
+        import tokenize
+        if hasattr(tokenize, 'open'):
+            # version 3.2
+            stream = tokenize.open(abs_path)  # @UndefinedVariable
+        else:
+            # version 3.0 or 3.1
+            detect_encoding = tokenize.detect_encoding(open(abs_path, mode="rb").readline)
+            stream = open(abs_path, encoding=detect_encoding[0])
+        try:
+            contents = stream.read()
+        finally:
+            stream.close()
 
+        # compile first to have the filename set in debug mode
+        _CACHE[abs_path] = compile(contents + "\n", abs_path, 'exec')
+
+    # execute the script from cache
+    exec(_CACHE[abs_path], glob, loc)
