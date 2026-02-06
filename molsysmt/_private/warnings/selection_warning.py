24013a84bd813eb6ad2726819124a2bc00c43c4c
Diego Prada <prada.gracia@gmail.com>
2026-02-05 21:54:59 -0600

    Flatten legacy warnings and exceptions modules

--- molsysmt/_private/warnings/selection_warning.py
+++
@@ -1,5 +0,0 @@
-from .user_molsysmt_warning import UserMolSysMTWarning
-
-class SelectionWarning(UserMolSysMTWarning):
-    """Warnings related to selection strings and resolved subsets."""
-    pass
