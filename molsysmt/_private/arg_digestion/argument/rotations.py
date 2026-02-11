fc14aa7d773578f12384798330320e320ba2c2e2
Diego Prada <prada.gracia@gmail.com>
2026-02-10 19:27:43 -0600

    Fix bioassembly digestion wiring for chain indices, rotations and translations

--- molsysmt/_private/arg_digestion/argument/rotations.py
+++ molsysmt/_private/arg_digestion/argument/rotations.py
@@ -1,15 +1,13 @@
 import numpy as np
-from molsysmt import pyunitwizard as puw
 from molsysmt._private.smonitor import ArgumentError
+from .rotation import digest_rotation
 
 def digest_rotations(rotations, caller=None):
 
     if caller is not None:
         if caller.endswith('digest_bioassembly'):
-            from .rotation import arg_digest_rotation
             if isinstance(rotations, (np.ndarray, list, tuple)):
                 return [digest_rotation(ii) for ii in rotations]
 
-    raise ArgumentError('rotations', value=rotation, caller=caller, message=None)
-
+    raise ArgumentError('rotations', value=rotations, caller=caller, message=None)
 
