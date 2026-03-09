aebb6bf3f08652da3f09406b436d2401eefa88e8
Diego Prada <prada.gracia@gmail.com>
2026-03-09 17:03:26 -0600

    Refactor native rebuild hierarchy away from form-agnostic queries

--- molsysmt/element/entity/get_entity_name.py
+++ molsysmt/element/entity/get_entity_name.py
@@ -6,18 +6,59 @@ import numpy as np
 def get_entity_name(molecular_system, element='entity', selection='all', redefine_indices=False,
                     redefine_names=False, syntax='MolSysMT', skip_digestion=False):
 
-    if redefine_indices:
+    if redefine_indices or redefine_names:
 
-        raise NotImplementedError
+        from ..molecule import get_molecule_name, get_molecule_type, get_molecule_index
 
-    elif redefine_names:
+        if redefine_indices:
+            redefine_names = True
+            redefine_types = True
+        else:
+            redefine_types = False
 
-        from ..molecule import get_molecule_name
+        molecule_index_from_atoms = get_molecule_index(molecular_system, element='atom',
+                selection=selection, redefine_indices=redefine_indices, syntax=syntax, skip_digestion=True)
 
-        molecule_name_from_entities = get_molecule_name(molecular_system, element='entity',
-                selection=selection, redefine_names=True, syntax=syntax, skip_digestion=True)
+        molecule_name_from_molecules = get_molecule_name(molecular_system, element='molecule',
+                selection='all', redefine_indices=redefine_indices, redefine_names=redefine_names,
+                syntax=syntax, skip_digestion=True)
 
-        output = [ii[0] for ii in molecule_name_from_entities]
+        molecule_type_from_molecules = get_molecule_type(molecular_system, element='molecule',
+                selection='all', redefine_indices=redefine_indices, redefine_types=redefine_types,
+                syntax=syntax, skip_digestion=True)
+
+        count = 0
+        molecule_to_entity = []
+        entity_names = {}
+        aux_dict = {}
+
+        for molecule_name, molecule_type in zip(molecule_name_from_molecules, molecule_type_from_molecules):
+
+            if molecule_type == 'water':
+                key = 'water'
+            else:
+                key = molecule_name
+
+            if key not in aux_dict:
+                aux_dict[key] = count
+                entity_index = count
+                entity_names[count] = key
+                count += 1
+            else:
+                entity_index = aux_dict[key]
+
+            molecule_to_entity.append(entity_index)
+
+        match element:
+            case 'atom':
+                output=[entity_names[molecule_to_entity[ii]] for ii in molecule_index_from_atoms]
+            case 'molecule':
+                output=[entity_names[ii] for ii in molecule_to_entity]
+            case 'entity':
+                n_entities = len(entity_names)
+                output=[entity_names[ii] for ii in range(n_entities)]
+            case _:
+                raise ValueError(f"Element '{element}' is not supported.")
 
     else:
 
@@ -26,4 +67,3 @@ def get_entity_name(molecular_system, element='entity', selection='all', redefin
                      entity_name=True, skip_digestion=True)
 
     return output
-
