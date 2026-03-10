cd81c7f18f92cdb1852a2e7e24a218350fba02c9
Diego Prada <prada.gracia@gmail.com>
2026-03-09 22:52:31 -0600

    Complete MDAnalysis AtomGroup adapter basics

--- molsysmt/form/MDAnalysis_AtomGroup/__init__.py
+++ molsysmt/form/MDAnalysis_AtomGroup/__init__.py
@@ -2,9 +2,9 @@ form_name = 'MDAnalysis.AtomGroup'
 form_type = 'class'
 form_info = ["", ""]
 
-piped_topological_attribute = None
-piped_structural_attribute = None
-piped_any_attribute = None
+piped_topological_attribute = 'molsysmt.Topology'
+piped_structural_attribute = 'molsysmt.MolSys'
+piped_any_attribute = 'molsysmt.MolSys'
 bonds_are_explicit = True
 bonds_can_be_computed = True
 
--- molsysmt/form/MDAnalysis_AtomGroup/get_topological_attributes.py
+++ molsysmt/form/MDAnalysis_AtomGroup/get_topological_attributes.py
@@ -1,8 +1,16 @@
 from molsysmt._private.arg_digestion import arg_digest
-from molsysmt._private.variables import is_all
 import types
 
-form='MDAnalysis.AtomGroup'
-# List of functions to be imported
-import types
+form = 'MDAnalysis.AtomGroup'
+
+
+@arg_digest(form=form)
+def get_atom_name_from_atom(item, indices='all', skip_digestion=False):
+    from .to_molsysmt_Topology import to_molsysmt_Topology
+    from molsysmt.form.molsysmt_Topology.get_topological_attributes import get_atom_name_from_atom as aux_get
+
+    tmp_item = to_molsysmt_Topology(item, skip_digestion=True)
+    return aux_get(tmp_item, indices=indices, skip_digestion=True)
+
+
 __all__ = [name for name, obj in globals().items() if isinstance(obj, types.FunctionType) and name.startswith('get_')]
