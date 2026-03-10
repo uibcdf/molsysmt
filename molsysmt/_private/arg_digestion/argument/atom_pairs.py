7294042cc98cadb783f1c09f8ddd64eeb84cd12a
Diego Prada <prada.gracia@gmail.com>
2026-03-10 12:16:52 -0600

    Add distributed coverage for collection argument digesters

--- molsysmt/_private/arg_digestion/argument/atom_pairs.py
+++ molsysmt/_private/arg_digestion/argument/atom_pairs.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def digest_atom_pairs(atom_pairs, caller=None):
 
-    if caller.endswith('add_harmonic_bond_force'):
+    if caller is not None and caller.endswith('add_harmonic_bond_force'):
         if is_iterable_of_integers(atom_pairs):
             if isinstance(atom_pairs, (list,tuple)) and len(atom_pairs) == 2:
                 return [[atom_pairs[0], atom_pairs[1]]]
@@ -12,7 +12,7 @@ def digest_atom_pairs(atom_pairs, caller=None):
                 return [[atom_pairs[0], atom_pairs[1]]]
         elif is_iterable_of_pairs(atom_pairs):
             return [[ii, jj] for ii,jj in atom_pairs]
-    elif caller.endswith('add_contacts'):
+    elif caller is not None and caller.endswith('add_contacts'):
         if atom_pairs is None:
             return None
         if is_iterable_of_integers(atom_pairs):
@@ -27,4 +27,3 @@ def digest_atom_pairs(atom_pairs, caller=None):
                 return atom_pairs
 
     raise ArgumentError('atom_pairs', value=atom_pairs, caller=caller, message=None)
-
