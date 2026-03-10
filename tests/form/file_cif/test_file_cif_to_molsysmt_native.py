e02c0e4fbb4fbbda1568b160aa40912702e653fd
Diego Prada <prada.gracia@gmail.com>
2026-03-09 19:06:24 -0600

    Add native conversion coverage for bcif and cif file forms

---
+++ tests/form/file_cif/test_file_cif_to_molsysmt_native.py
@@ -0,0 +1,25 @@
+"""Regression tests for file:cif conversions to native MolSysMT forms."""
+
+import numpy as np
+import molsysmt as msm
+
+
+def test_file_cif_to_molsysmt_topology_preserves_counts(hp35_cif_file):
+    topology = msm.convert(hp35_cif_file, to_form='molsysmt.Topology')
+
+    assert topology.n_atoms == 596
+    assert topology.n_groups == 36
+    assert topology.n_entities == 1
+
+
+def test_file_cif_to_molsysmt_molsys_preserves_first_names(hp35_cif_file):
+    molsys = msm.convert(hp35_cif_file, to_form='molsysmt.MolSys')
+
+    assert np.all(molsys.topology.atoms['atom_name'].to_numpy()[:5] == np.array(['N', 'CA', 'C', 'O', 'CB'], dtype=object))
+    assert np.all(molsys.topology.groups['group_name'].to_numpy()[:3] == np.array(['MET', 'LEU', 'SER'], dtype=object))
+
+
+def test_file_cif_to_molsysmt_molsys_preserves_explicit_entity_ids(hp35_cif_file):
+    molsys = msm.convert(hp35_cif_file, to_form='molsysmt.MolSys')
+
+    assert molsys.topology.entities['entity_id'].to_list() == ['1']
