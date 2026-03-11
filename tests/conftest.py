1379b82cd269357d735342a13fee54eb6c7f81a0
Diego Prada <prada.gracia@gmail.com>
2026-03-11 00:04:51 -0600

    Use MolSysBuilder fixtures for deterministic PDB converter tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -133,6 +133,65 @@ def _base_md_1u19_pdb_text():
     assert pdb_text is not None
     return pdb_text
 
+
+@pytest.fixture(scope="session")
+def _base_builder_pdb_molsys():
+    from molsysmt import pyunitwizard as puw
+    import numpy as np
+
+    builder = msm.MolSysBuilder()
+    atom_indices = [builder.add_atom(atom_name=name) for name in ["N", "CA", "C", "O"]]
+    group_index_0 = builder.add_group(atom_indices[:3], group_id="10", group_name="ALA")
+    group_index_1 = builder.add_group(atom_indices[3:], group_id="11", group_name="HOH")
+    builder.add_bond(atom_indices[0], atom_indices[1])
+    builder.add_bond(atom_indices[1], atom_indices[2])
+    builder.add_chain([group_index_0, group_index_1], chain_id="A", chain_name="A")
+    molecule_index_0 = builder.add_molecule([group_index_0], molecule_id="20", molecule_name="protein 0", molecule_type="protein")
+    molecule_index_1 = builder.add_molecule([group_index_1], molecule_id="21", molecule_name="water", molecule_type="water")
+    builder.add_entity([molecule_index_0], entity_id="30", entity_name="protein 0", entity_type="protein")
+    builder.add_entity([molecule_index_1], entity_id="31", entity_name="water", entity_type="water")
+    builder.set_coordinates(
+        puw.quantity(
+            np.array(
+                [
+                    [0.0, 0.0, 0.0],
+                    [0.1, 0.0, 0.0],
+                    [0.2, 0.0, 0.0],
+                    [0.3, 0.0, 0.0],
+                ]
+            ),
+            "nm",
+        )
+    )
+    builder.set_box(puw.quantity(np.eye(3)[np.newaxis, :, :], "nm"))
+    builder.set_time(puw.quantity(np.array([0.0]), "ps"))
+    builder.set_structure_id([0])
+    molsys = builder.build()
+    assert molsys is not None
+    return molsys
+
+
+@pytest.fixture(scope="session")
+def _base_builder_pdb_text(_base_builder_pdb_molsys):
+    pdb_text = msm.convert(_base_builder_pdb_molsys, to_form='string:pdb_text')
+    assert pdb_text is not None
+    return pdb_text
+
+
+@pytest.fixture()
+def builder_pdb_molsys(_base_builder_pdb_molsys):
+    return _base_builder_pdb_molsys.copy()
+
+
+@pytest.fixture()
+def builder_pdb_text(_base_builder_pdb_text):
+    return _base_builder_pdb_text
+
+
+@pytest.fixture()
+def builder_pdb_handler(builder_pdb_text):
+    return msm.convert(builder_pdb_text, to_form='molsysmt.PDBFileHandler')
+
 # TcTIM systems
 
 @pytest.fixture(scope="session")
