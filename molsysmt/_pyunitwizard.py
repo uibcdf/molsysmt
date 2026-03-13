aa29bb45d74bdb987f8fa1dc08dae83324a6fd81
Diego Prada <prada.gracia@gmail.com>
2026-03-12 22:14:12 -0600

    Configuration: Registered MolSysMT canonical units in puw.fast_track.

--- molsysmt/_pyunitwizard.py
+++ molsysmt/_pyunitwizard.py
@@ -7,3 +7,8 @@ puw.configure.set_default_parser('pint')
 puw.configure.set_standard_units(['nm', 'ps', 'K', 'mole', 'amu', 'e',
                                  'kJ/mol', 'kJ/(mol*nm)', 'kJ/(mol*nm**2)', 'radians'])
 # puw.configure.add_constant(name, value, unit)
+
+# Registering MolSysMT canonical fast-tracks
+puw.register_fast_track("nanometers", puw.unit("nm"))
+puw.register_fast_track("picoseconds", puw.unit("ps"))
+puw.register_fast_track("kelvin", puw.unit("K"))
