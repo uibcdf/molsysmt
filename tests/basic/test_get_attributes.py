f7c96da8e894ef1cff241ed7144be65f7779d494
Diego Prada <prada.gracia@gmail.com>
2026-05-23 02:34:55 -0600

    test: fix get_attributes default format test and add default list test [skip ci]

--- tests/basic/test_get_attributes.py
+++ tests/basic/test_get_attributes.py
@@ -9,31 +9,39 @@ import numpy as np
 
 def test_get_attributes_string_pdb():
     molsys = '181L'
-    attributes = msm.get_attributes(molsys)
+    attributes = msm.get_attributes(molsys, output_type='dictionary')
     assert attributes['group_name']==True
     assert attributes['entity_name']==True
     assert attributes['box']==True
 
 def test_get_attributes_molsysmt_MolSys():
     molsys = msm.convert(systems['T4 lysozyme L99A']['181l.h5msm'])
-    attributes = msm.get_attributes(molsys)
+    attributes = msm.get_attributes(molsys, output_type='dictionary')
     assert attributes['group_name']==True
     assert attributes['entity_name']==True
     assert attributes['box']==True
 
 def test_get_attributes_openmm_Topology():
     molsys = msm.convert(systems['T4 lysozyme L99A']['181l.h5msm'], to_form='openmm.Topology')
-    attributes = msm.get_attributes(molsys)
+    attributes = msm.get_attributes(molsys, output_type='dictionary')
     assert attributes['group_name']==True
     assert attributes['entity_name']==False
     assert attributes['box']==True
 
 def test_get_attributes_string_amino_acids_1():
     molsys = msm.convert(systems['T4 lysozyme L99A']['181l.h5msm'], to_form='string:amino_acids_1', selection='molecule_type=="protein"')
-    attributes = msm.get_attributes(molsys)
+    attributes = msm.get_attributes(molsys, output_type='dictionary')
     assert attributes['group_name']==True
     assert attributes['entity_name']==False
     assert attributes['box']==False
+
+def test_get_attributes_default_list():
+    molsys = '181L'
+    attributes = msm.get_attributes(molsys)
+    assert isinstance(attributes, list)
+    assert 'group_name' in attributes
+    assert 'box' in attributes
+
 
 
 
