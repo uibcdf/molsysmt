b1e95f15e30c28b2d78c0dffc4a8405e8dba9789
Diego Prada <prada.gracia@gmail.com>
2026-08-16 03:18:16 -0600

    fix(nglview): resolve nglview_htmlfile path via MSM_DOCS_NOTEBOOK, expose load_html_in_jupyter_notebook, and display NGLView iframe in view.ipynb [skip ci]

--- molsysmt/basic/viewer/nglview.py
+++ molsysmt/basic/viewer/nglview.py
@@ -10,12 +10,35 @@ def view(molecular_system=None, selection='all', structure_indices='all', syntax
          skip_digestion=False):
 
     if os.environ.get("MSM_VIEWS_FROM_HTML_FILES", "").lower() == "true":
-        if 'nglview_htmlfile' in stack()[2][0].f_locals:
-            htmlfile = stack()[2][0].f_locals['nglview_htmlfile']
-            if htmlfile is not None:
-                if Path(htmlfile).is_file():
-                    from molsysmt.third_party.nglview import load_html_in_jupyter_notebook
-                    return load_html_in_jupyter_notebook(htmlfile)
+        htmlfile = None
+        f_locals = None
+        for frame_info in stack():
+            f_locals = frame_info.frame.f_locals
+            if 'nglview_htmlfile' in f_locals or 'molsysviewer_htmlfile' in f_locals:
+                htmlfile = f_locals.pop('nglview_htmlfile', None) or f_locals.pop('molsysviewer_htmlfile', None)
+                break
+
+        resolved_path = None
+        if htmlfile is not None:
+            if Path(htmlfile).is_file():
+                resolved_path = htmlfile
+            else:
+                nb_env = os.environ.get("MSM_DOCS_NOTEBOOK")
+                if nb_env:
+                    nb_p = Path(nb_env).resolve()
+                    for p in [nb_p] + list(nb_p.parents):
+                        candidate = (p / htmlfile).resolve()
+                        if candidate.is_file():
+                            resolved_path = htmlfile
+                            break
+                        candidate2 = (p / "docs" / htmlfile).resolve()
+                        if candidate2.is_file():
+                            resolved_path = htmlfile
+                            break
+
+        if resolved_path is not None:
+            from molsysmt.third_party.nglview.load_html_in_jupyter_notebook import load_html_in_jupyter_notebook
+            return load_html_in_jupyter_notebook(str(resolved_path))
 
     if molecular_system is None:
         import nglview as nv
